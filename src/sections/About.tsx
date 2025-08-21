719842b78d866acbc86d1fccf84581e41a1f6f67
aniket1251 <ag125aa@gmail.com>
2025-08-21 08:53:27 +0530

    create CardHeader component

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -24,6 +24,13 @@ import { RiNextjsFill } from "react-icons/ri";
 import { VscVscode } from "react-icons/vsc";
 import { FaAws } from "react-icons/fa";
 import TechIcon from "@/components/TechIcon";
+import StarIcon from '@/assets/icons/star.svg';
+import Card from "@/components/Card";
+import Image from "next/image";
+import bookImage from "@/assets/images/TAGR.png";
+import mapImage from "@/assets/images/chandausi.png";
+import smileImage from "@/assets/images/memoji-smile.png";
+import CardHeader from "@/components/CardHeader";
 
 
 const techIcons = [
@@ -119,18 +126,91 @@ const techIcons = [
     title:"VS Code",
     icon:VscVscode,
   },
+];
+
+const hobbies =[
+  {
+    title:"Sports",
+    emoji:"🏏",
+  },
+  {
+    title:"Cinephile",
+    emoji:"🎬",
+  },
+  {
+    title:"Traveling",
+    emoji:"🌍",
+  },
+  {
+    title:"Reading",
+    emoji:"📚",
+  },
+  {
+    title:"Music",
+    emoji:"🎶",
+  },
+  {
+    title:"Fitness",
+    emoji:"💪",
+  },
+  {
+    title:"Photography",
+    emoji:"📸",
+  },
 ]
 
 export const AboutSection = () => {
-  return( 
-    <div>
-      <SectionHeader
-        eyebrow="About Me"
-        title="A Glimpse Into My World"
-        description="Learn more about who I am, what I do, and what inspires me."
-      />
-      <div>
-        <TechIcon icon={SiReact} label="React"/>
+  return(
+    <div className="py-20">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimpse Into My World"
+          description="Learn more about who I am, what I do, and what inspires me."
+        />
+        <div className="mt-20">
+          <Card className="h-[320px]">
+            <CardHeader title="My Reads" description="Explore the books shaping my perspectives."/>
+            <div className="w-40 mx-auto mt-8">
+              <Image src={bookImage} alt="book"/>
+            </div>
+          </Card>
+          <Card>
+            <div>
+              <StarIcon/>
+              <h3>My Toolbox</h3>
+              <p>Explore the technologies and tools I use to craft exceptional digital experiences.</p>
+            </div>
+            <div>
+              {techIcons.map((item, idx)=>(
+                <div key={idx}>
+                  <TechIcon icon={item.icon} label={item.title}/>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <div>
+              <StarIcon/>
+              <h3>Beyond the code</h3>
+              <p>Explore the books shaping my perspectives.</p>
+            </div>
+            <div>
+              {hobbies.map((hobby,idx)=>(
+                <div key={idx}>
+                  <span>{hobby.title}</span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <div>
+              <Image src={mapImage} alt="map"/>
+              <Image src={smileImage} alt="face-emoji"/>
+            </div>
+          </Card>
+        </div>
       </div>
     </div>
   );
