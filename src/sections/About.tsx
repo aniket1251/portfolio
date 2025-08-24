1191f401d12b3eac18d84c9d41ff04261f4c04c3
aniket1251 <ag125aa@gmail.com>
2025-08-24 09:08:57 +0530

    finish aboutSection

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -58,6 +58,30 @@ const techIcons1 = [
     title:"Postgresql",
     icon:BiLogoPostgresql,
   },
+    {
+    title:"AWS",
+    icon:FaAws,
+  },
+  {
+    title:"Git",
+    icon:SiGit,
+  },
+  {
+    title:"Github",
+    icon:SiGithub,
+  },
+  {
+    title:"Docker",
+    icon:SiDocker,
+  },
+  {
+    title:"Postman",
+    icon:SiPostman,
+  },
+  {
+    title:"VS Code",
+    icon:VscVscode,
+  },
 ];
 
 const techIcons2 = [
@@ -85,9 +109,7 @@ const techIcons2 = [
     title:"Mongodb",
     icon:SiMongodb,
   },
-]
-const techIcons3 = [
-  {
+    {
     title:"HTML5",
     icon:SiHtml5,
   },
@@ -112,38 +134,12 @@ const techIcons3 = [
     icon:SiMongoose,
   },
 ]
-const techIcons4 = [
-  {
-    title:"AWS",
-    icon:FaAws,
-  },
-  {
-    title:"Git",
-    icon:SiGit,
-  },
-  {
-    title:"Github",
-    icon:SiGithub,
-  },
-  {
-    title:"Docker",
-    icon:SiDocker,
-  },
-  {
-    title:"Postman",
-    icon:SiPostman,
-  },
-  {
-    title:"VS Code",
-    icon:VscVscode,
-  },
-]
 
 
 const hobbies =[
   {
-    title:"Sports",
-    emoji:"🏏",
+    title:"Traveling",
+    emoji:"🌍",
     left:"5%",
     top:"5%",
   },
@@ -154,8 +150,8 @@ const hobbies =[
     top:"5%",
   },
   {
-    title:"Traveling",
-    emoji:"🌍",
+    title:"Sports",
+    emoji:"🏏",
     left:"35%",
     top:"40%",
   },
@@ -187,7 +183,7 @@ const hobbies =[
 
 export const AboutSection = () => {
   return(
-    <div className="py-20">
+    <div className="py-20 lg:py-28">
       <div className="container">
         <SectionHeader
           eyebrow="About Me"
@@ -195,23 +191,24 @@ export const AboutSection = () => {
           description="Learn more about who I am, what I do, and what inspires me."
         />
         <div className="mt-20 flex flex-col gap-8">
-          <Card className="h-[320px]">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+          <Card className="h-[320px] md:col-span-2 lg:col-span-1">
             <CardHeader title="My Reads" description="Explore the books shaping my perspectives."
             />
-            <div className="w-40 mx-auto mt-2">
-              <Image src={bookImage} alt="book" className="mt-4"/>
+            <div className="w-40 mx-auto mt-0">
+              <Image src={bookImage} alt="book" className="-mt-2 md:-mt-4 lg:-mt-6"/>
             </div>
           </Card>
-          <Card className="h-[450px] p-0">
+          <Card className="h-[320px] md:col-span-3 lg:col-span-2">
             <CardHeader title="My Toolbox" description="Explore the technologies and tools I use to craft exceptional digital experiences."
-            className="px-6 pt-6"
+            className=""
             />
-            <ToolboxItems techIcons={techIcons1} className="mt-6"/>
+            <ToolboxItems techIcons={techIcons1} className=""/>
             <ToolboxItems techIcons={techIcons2} className="mt-6"/>
-            <ToolboxItems techIcons={techIcons3} className="mt-6"/>
-            <ToolboxItems techIcons={techIcons4} className="mt-6"/>
           </Card>
-          <Card className="h-[320px] p-0 flex flex-col">
+          </div>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+          <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
             <CardHeader title="Beyond the code" description="Explore the books shaping my perspectives." className="px-6 py-6"
             />
             <div className="relative flex-1">
@@ -226,12 +223,13 @@ export const AboutSection = () => {
               ))}
             </div>
           </Card>
-          <Card className="h-[320px] p-0 relative">
+          <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
             <Image src={mapImage} alt="map" className="h-full w-full object-cover"/>
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-10 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:outline-offset-2 after:rounded-full after:outline-gray-950/30">
               <Image src={smileImage} alt="face-emoji" className="size-10"/>
             </div>
           </Card>
+          </div>
         </div>
       </div>
     </div>
