9c01b2b2984eba58894ec6a3bd9ddca3973911c9
aniket1251 <ag125aa@gmail.com>
2025-08-25 13:43:20 +0530

    fix navbar covering content issue

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,19 +1,35 @@
 export const Header = () => {
+const handleScroll = (id: string) => {
+  const element = document.getElementById(id);
+  if (element) {
+    const headerOffset = 10;
+    const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  }
+};
+
+
+
   return (
   <div className="flex justify-center items-center fixed top-3 w-full z-10">
     <nav className="flex p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-      <a href="#home" className="nav-item">
+      <button onClick={() => handleScroll("home")} className="nav-item">
         Home
-      </a>
-      <a href="#projects" className="nav-item">
+      </button>
+      <button onClick={() => handleScroll("projects")} className="nav-item">
         Projects
-      </a>
-      <a href="#about" className="nav-item">
+      </button>
+      <button onClick={() => handleScroll("about")} className="nav-item">
         About
-      </a>
-      <a href="#contact" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
+      </button>
+      <button onClick={() => handleScroll("contact")} className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
         Contact
-      </a>
+      </button>
     </nav>
   </div>
   );
